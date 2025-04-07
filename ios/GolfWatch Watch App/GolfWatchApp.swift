95dd4d4e146e8a5054981c9ba6f295f5ed24d03c
Daniel <245dany@gmail.com>
2025-04-07 08:46:22 +0200

    watch app

---
+++ ios/GolfWatch Watch App/GolfWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfWatchApp.swift
+//  GolfWatch Watch App
+//
+//  Created by dev on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GolfWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
